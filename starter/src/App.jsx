60a742662307ca30183b0996aeba8ac57bce0b1f
emmeiwhite <emmeiwhite@gmail.com>
2024-10-29 22:02:47 +0530

    custom instance with axios

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,22 +1,40 @@
-import { ToastContainer } from 'react-toastify';
-import { nanoid } from 'nanoid';
-import Form from './Form';
-import Items from './Items';
-import { useState } from 'react';
+import { ToastContainer } from 'react-toastify'
+import { nanoid } from 'nanoid'
+import Form from './Form'
+import Items from './Items'
+import { useEffect, useState } from 'react'
+import fetchCustom from './utils'
+
 const defaultItems = [
   { id: nanoid(), title: 'walk the dog', isDone: false },
   { id: nanoid(), title: 'wash dishes', isDone: false },
   { id: nanoid(), title: 'drink coffee', isDone: true },
-  { id: nanoid(), title: 'take a nap', isDone: false },
-];
+  { id: nanoid(), title: 'take a nap', isDone: false }
+]
+
 const App = () => {
-  const [items, setItems] = useState(defaultItems);
+  const [items, setItems] = useState(defaultItems)
+
+  const fetchData = async () => {
+    try {
+      const { data } = await fetchCustom()
+
+      console.log(data)
+      setItems(data.taskList)
+    } catch (error) {}
+  }
+
+  // To perform side effects, fetch, DOM manipulations etc, using some service
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
-    <section className='section-center'>
-      <ToastContainer position='top-center' />
+    <section className="section-center">
+      <ToastContainer position="top-center" />
       <Form />
       <Items items={items} />
     </section>
-  );
-};
-export default App;
+  )
+}
+export default App
